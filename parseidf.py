5af3a85d09b925fb6e4447b8a03f61565335d3a9
katsuya0719 <katsuya0719@gmail.com>
2017-08-06 18:15:16 +0900

    will plot extracted information

--- parseidf.py
+++ parseidf.py
@@ -1,5 +1,6 @@
 from eppy import modeleditor
 from eppy.modeleditor import IDF
+from .chiller import visBiquadratic
 
 class parseIDF():
     def __init__(self,idf):
@@ -11,7 +12,7 @@ class parseIDF():
     def readobjs(self,objs):
         """
         read specified objects from imported idf file and store in list
-        :param objs:
+        :param objs:specify objects user want to extract
         :return:
         """
         idf=self.idf
@@ -22,15 +23,34 @@ class parseIDF():
 
         self.objects=objlist
 
+    def readattrs(self,objid,attrs):
+        objs=self.objects[objid]
+        objList=[]
+        for obj in objs:
+            attrList=[]
+            for attr in attrs:
+                print(obj[attr])
+                attrList.append(obj[attr])
+
+            objList.append(attrList)
+
+        return objList
+
+    def plot(self,objs):
+
+
 if __name__ == '__main__':
     idf="data/EP/idf/AirCooledChiller.idf"
     #idf="data/EP/idf/smallidf.idf"
     objects=['Chiller:Electric:EIR','Curve:Biquadratic','Curve:Quadratic']
+    attributes=["Name","Coefficient1_Constant","Coefficient2_x","Coefficient3_x2","Coefficient4_y","Coefficient5_y2","Coefficient6_xy"]
+    #attributes = [Name, Coefficient1 Constant, Coefficient2 x, Coefficient3 x2, Coefficient4 y,Coefficient5 y2, Coefficient6 xy]
     parsed=parseIDF(idf)
     #print (parsed.idf.printidf())
     #print(parsed.idf.idfobjects['Chiller:Electric:EIR'.upper()])
     parsed.readobjs(objects)
 
-    print(parsed.objects)
+    biquadratic=parsed.readattrs(1,attributes)
+    print(biquadratic)
 
 
